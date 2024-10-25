603abbbcd58c03dbbd2c8a3d4304821b777840eb
abimriyansah22 <abimriyansah22@gmail.com>
2024-10-25 08:56:36 +0700

    Add files via upload

---
+++ inpu string abim.py
@@ -0,0 +1,3 @@
+nama = input('Masukan nama: ')
+
+print('Halo, ' , nama, '. . selamat datang: ')
---
+++ string luas pp Abim.py
@@ -0,0 +1,5 @@
+print('kalkulator luas persegi panjang\n')
+panjang = input('Masukan panjang: ')
+lebar = input('Masukan lebar: ')
+
+print('Luas =' , int (panjang)  * int (lebar))
